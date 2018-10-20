3875ebfb37812cd1f37cc688487563e44759f520
bsmay <bsmay44@gmail.com>
2018-10-20 13:54:58 -0400

    added pop-up windows

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-let age; 
-console.log("age is currently: "+ age"); 
-age=30;
-console.log("age is currently: "+ age"); 
-age = 30 + 1;
+let name =prompt("Please state your name."); 
+
+let pancakeConfirm =confirm("Please press OK " +  name  +  " if you cook pancakes regularly.");
+
+if (pancakeConfirm){
+    alert("Welcome to my page " + name +"!");
+}
+
+else {
+     alert("Please leave immediately.");
+}
+
+console.log("The user's name is" + name)
+
+
+
